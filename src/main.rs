baf76a503afd875e7dd9b74bacd1e4c530f05f7b
Naoki Aoyama <9667078+naa0yama@users.noreply.github.com>
2025-08-26 17:54:21 +0900

    chore: add custom printIn
    Signed-off-by: Naoki Aoyama <9667078+naa0yama@users.noreply.github.com>

--- src/main.rs
+++ src/main.rs
@@ -3,15 +3,31 @@ use crate::libs::hello::sayhello;
 use clap::Parser;
 
 #[derive(Parser)]
-#[command(version, about)]
+#[command(about)]
 struct Args {
     /// Name of the person to greet
     #[arg(short, long, default_value = "Youre")]
     name: String,
+    #[arg(long, short = 'V', help = "Print version")]
+    version: bool,
 }
+
+const APP_VERSION: &str = concat!(
+    env!("CARGO_PKG_NAME"),
+    " version ",
+    env!("CARGO_PKG_VERSION"),
+    " (rev:",
+    env!("GIT_HASH"),
+    ")\n",
+);
 
 fn main() {
     let args = Args::parse();
+    if args.version {
+        print!("{}", APP_VERSION);
+        std::process::exit(0);
+    }
+
     run(args.name);
 }
 
