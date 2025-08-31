88ae99e19f8e5593ced18d77d4d48c197c37dc4b
Naoki Aoyama <9667078+naa0yama@users.noreply.github.com>
2025-08-31 19:34:25 +0900

    chore: add tracing
    Signed-off-by: Naoki Aoyama <9667078+naa0yama@users.noreply.github.com>

--- tests/integration_test.rs
+++ tests/integration_test.rs
@@ -1,10 +1,5 @@
 use assert_cmd::Command;
-
-#[test]
-fn test_cli_with_default_name() {
-    let mut cmd = Command::cargo_bin("brust").unwrap();
-    cmd.assert().success().stdout("Hi, Youre, new world!!\n");
-}
+use predicates::prelude::*;
 
 #[test]
 fn test_cli_with_custom_name() {
@@ -13,7 +8,7 @@ fn test_cli_with_custom_name() {
         .arg("Alice")
         .assert()
         .success()
-        .stdout("Hi, Alice, new world!!\n");
+        .stdout(predicate::str::contains("Hi, Alice, new world!!"));
 }
 
 #[test]
@@ -23,41 +18,23 @@ fn test_cli_with_short_flag() {
         .arg("Bob")
         .assert()
         .success()
-        .stdout("Hi, Bob, new world!!\n");
-}
-
-#[test]
-fn test_cli_with_japanese_name() {
-    let mut cmd = Command::cargo_bin("brust").unwrap();
-    cmd.arg("--name")
-        .arg("世界")
-        .assert()
-        .success()
-        .stdout("Hi, 世界, new world!!\n");
-}
-
-#[test]
-fn test_cli_with_empty_name() {
-    let mut cmd = Command::cargo_bin("brust").unwrap();
-    cmd.arg("--name")
-        .arg("")
-        .assert()
-        .success()
-        .stdout("Hi, , new world!!\n");
+        .stdout(predicate::str::contains("Hi, Bob, new world!!"));
 }
 
 #[test]
 fn test_cli_version_flag() {
     let mut cmd = Command::cargo_bin("brust").unwrap();
-    let output = cmd.arg("--version").output().unwrap();
-    let stdout = String::from_utf8(output.stdout).unwrap();
-    assert!(stdout.starts_with("brust version"));
+    cmd.arg("--version")
+        .assert()
+        .success()
+        .stdout(predicate::str::contains("brust version"));
 }
 
 #[test]
 fn test_cli_version_short_flag() {
     let mut cmd = Command::cargo_bin("brust").unwrap();
-    let output = cmd.arg("-V").output().unwrap();
-    let stdout = String::from_utf8(output.stdout).unwrap();
-    assert!(stdout.starts_with("brust version"));
+    cmd.arg("-V")
+        .assert()
+        .success()
+        .stdout(predicate::str::contains("brust version"));
 }
